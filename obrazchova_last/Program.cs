6dc811b6c2bee8f1c5d25317f6e83ba35356c383
Arefiev Yuriy Vladimirovich <delurya22@gmail.com>
2024-03-19 23:09:33 +0300

    v1

---
+++ obrazchova_last/Program.cs
@@ -0,0 +1,49 @@
+using NLog;
+using NLog.Web;
+using Microsoft.EntityFrameworkCore;
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+logger.Debug("My API starts");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception exception)
+{
+    logger.Error(exception, "My API stop ");
+    throw;
+
+}
+finally { 
+
+    NLog.LogManager.Shutdown();
+}
